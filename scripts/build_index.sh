2d07066200f2c53043efc6c69dd3c378b2aee4aa
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-06-28 22:15:29 +0500

    ADDED: bash script to run entire ingestion

--- scripts/build_index.sh
+++ scripts/build_index.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-# Run ingestion and build both dense and sparse indexes
-python src/indexing/build_index.py
+
+
+# Build the index for the evaluation suite
+python -m src.indexing.build_index "$@"
